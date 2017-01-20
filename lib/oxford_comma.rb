c58c3edf12b457dd14493b6fa89da083ba452256
Martin Crane <martin.crane@gmail.com>
2017-01-20 18:48:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.size == 1
+    array.pop
+  elsif array.size == 2
+    array.shift + " and " + array.shift
+  elsif array.size == 3
+    array.shift + ", " + array.shift + ", and " + array.shift
+  else
+    sentence = ""
+    sentence = ", and " + array.pop
+    array.join(", ") + sentence
+  end
 
 end
